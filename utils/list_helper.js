6d4408e1b66756f70f295bc1a56270a19cf7a018
Nigouu <uggla.nicolas@gmail.com>
2021-09-01 12:20:28 +0300

    beter organized

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,19 +1,17 @@
-
-
 const dummy = (blogs) => {
   return 1
 }
 
+
 const totalLikes = (blogs) => {
   var sum = 0
-  // console.log('summan före loopen är', sum);
   for (var i = 0; i < blogs.length; i++){
       var addition = blogs[i].likes
       sum = sum + addition
-      // console.log('summan innanför loopen är', sum);
   }
   return sum
 }
+
 
 const favoriteBlog = (blogs) => {
   sum = 0
@@ -22,22 +20,18 @@ const favoriteBlog = (blogs) => {
     author:'',
     likes:0
   }
-  // console.log('summan före favorit loopen är', obj);
   for (var i = 0; i < blogs.length; i++){
       var addition = blogs[i].likes
-      // console.log('summan är', addition);
       if (addition > sum) {
         sum = addition
-        // console.log(sum, 'summan sätts inne i loopen om den är större');
         obj.title = blogs[i].title
         obj.author = blogs[i].author
         obj.likes = blogs[i].likes
-        // console.log('summan innanför loopen är', obj);
       }
   }
-  // console.log(obj, 'summan efter att allt är klart');
   return obj
 }
+
 
 const mostBlogs = (blogs) => {
   sum = 0
@@ -45,20 +39,17 @@ const mostBlogs = (blogs) => {
     author:'',
     blogsAmount:0
   }
-  // console.log(obj);
   for (var i = 0; i < blogs.length; i++){
       var addition = blogs[i].blogsAmount
-      // console.log('summan av nuvarande', addition);
       if (addition > sum) {
         sum = addition
         obj.author = blogs[i].author
         obj.blogsAmount = blogs[i].blogsAmount
-        // console.log('nuvarande objektet', obj);
       }
   }
-  // console.log('slutliga', obj);
   return obj
 }
+
 
 const mostLikes = (blogs) => {
   sum = 0
