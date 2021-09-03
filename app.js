63458c293d47acd1354c6dafefdd69ad06ae39e6
Nigouu <uggla.nicolas@gmail.com>
2021-09-03 16:37:35 +0300

    current situation

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+const usersRouter = require('./controllers/users')
+app.use('/api/users', usersRouter)
 
 logger.info('connecting to', config.MONGODB_URI)
 
